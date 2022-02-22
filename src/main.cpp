820aa2109543e4442a1d11aac96ae04f5930be0e
STadas <saltenis.tadas@gmail.com>
2022-02-22 17:24:54 +0200

    Remove signal ignores

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
 #include <QApplication>
 
-#include <signal.h>
-
 #include "widgets/MainWindow.hpp"
 
 int main(int argc, char *argv[])
 {
-    signal(SIGPIPE, SIG_IGN);
-
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
