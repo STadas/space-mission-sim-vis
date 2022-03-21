4c467f2d64fb19286c51a67f058fc8655e393d33
STadas <saltenis.tadas@gmail.com>
2022-03-21 20:48:06 +0200

    Add new command and comment tests

--- tests/src/PanguParser.cpp
+++ tests/src/PanguParser.cpp
@@ -23,6 +23,16 @@ TEST_F(PanguParserTest, ParsePanguEmptyString)
     ASSERT_FALSE(parsed.command.has_value());
 }
 
+TEST_F(PanguParserTest, ParsePanguComment)
+{
+    const QString str = "# foo bar";
+
+    auto parsed = parser.parse(str);
+
+    ASSERT_EQ(parsed.err, CommandErr::EMPTY);
+    ASSERT_FALSE(parsed.command.has_value());
+}
+
 TEST_F(PanguParserTest, ParsePanguNotImplementedCommand)
 {
     const QString str = "foo";
@@ -66,6 +76,26 @@ TEST_F(PanguParserTest, ParsePanguStartBadArgType)
 TEST_F(PanguParserTest, ParsePanguStartOK)
 {
     const QString str = "start 1 2 3 4 5 6";
+
+    auto parsed = parser.parse(str);
+
+    ASSERT_EQ(parsed.err, CommandErr::OK);
+    ASSERT_TRUE(parsed.command.has_value());
+}
+
+TEST_F(PanguParserTest, ParsePanguOKWithComment)
+{
+    const QString str = "start 1 2 3 4 5 6 # foo bar";
+
+    auto parsed = parser.parse(str);
+
+    ASSERT_EQ(parsed.err, CommandErr::OK);
+    ASSERT_TRUE(parsed.command.has_value());
+}
+
+TEST_F(PanguParserTest, ParsePanguOKWithCommentNoSpaceBetween)
+{
+    const QString str = "start 1 2 3 4 5 6# foo bar";
 
     auto parsed = parser.parse(str);
 
@@ -156,6 +186,76 @@ TEST_F(PanguParserTest, ParsePanguQuaternionOK)
 TEST_F(PanguParserTest, ParsePanguQuaternionDouble)
 {
     const QString str = "quaternion 1 2 3 4.012938102471098123213098213 5 6 7";
+
+    auto parsed = parser.parse(str);
+
+    ASSERT_EQ(parsed.err, CommandErr::OK);
+    ASSERT_TRUE(parsed.command.has_value());
+}
+
+TEST_F(PanguParserTest, ParsePanguUpdateTooManyArgs)
+{
+    const QString str = "update foo";
+
+    auto parsed = parser.parse(str);
+
+    ASSERT_EQ(parsed.err, CommandErr::BAD_ARG_COUNT);
+    ASSERT_FALSE(parsed.command.has_value());
+}
+
+TEST_F(PanguParserTest, ParsePanguUpdateOK)
+{
+    const QString str = "update";
+
+    auto parsed = parser.parse(str);
+
+    ASSERT_EQ(parsed.err, CommandErr::OK);
+    ASSERT_TRUE(parsed.command.has_value());
+}
+
+TEST_F(PanguParserTest, ParsePanguPauseNotEnoughArgs)
+{
+    const QString str = "pause";
+
+    auto parsed = parser.parse(str);
+
+    ASSERT_EQ(parsed.err, CommandErr::BAD_ARG_COUNT);
+    ASSERT_FALSE(parsed.command.has_value());
+}
+
+TEST_F(PanguParserTest, ParsePanguPauseTooManyArgs)
+{
+    const QString str = "pause 1 2";
+
+    auto parsed = parser.parse(str);
+
+    ASSERT_EQ(parsed.err, CommandErr::BAD_ARG_COUNT);
+    ASSERT_FALSE(parsed.command.has_value());
+}
+
+TEST_F(PanguParserTest, ParsePanguPauseBadArgType)
+{
+    const QString str = "pause foo";
+
+    auto parsed = parser.parse(str);
+
+    ASSERT_EQ(parsed.err, CommandErr::BAD_ARG_TYPE);
+    ASSERT_FALSE(parsed.command.has_value());
+}
+
+TEST_F(PanguParserTest, ParsePanguPauseOK)
+{
+    const QString str = "pause 1";
+
+    auto parsed = parser.parse(str);
+
+    ASSERT_EQ(parsed.err, CommandErr::OK);
+    ASSERT_TRUE(parsed.command.has_value());
+}
+
+TEST_F(PanguParserTest, ParsePanguPauseDouble)
+{
+    const QString str = "pause 1.2218379264329384692384729347";
 
     auto parsed = parser.parse(str);
 
