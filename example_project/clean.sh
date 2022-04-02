1e8550ec2bedad3a7d5f3e0264640cbe43091764
STadas <saltenis.tadas@gmail.com>
2022-04-02 16:13:29 +0300

    Add example project

---
+++ example_project/clean.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Remove generated files.
+echo "Removing generated files"
+/bin/rm -f _PERF_RESULTS.txt
+/bin/rm -f PANGU.log
+/bin/rm -f albedo_map.tga
+/bin/rm -f boulder_list.txt
+/bin/rm -f boulder_size_freq_dist.txt
+/bin/rm -f crater_list.txt
+/bin/rm -f crater_size_freq_dist.txt
+/bin/rm -f model.pan
+/bin/rm -f surface.dem
+/bin/rm -f surface.dem.txt
