6b5cd116e20913293910f5ac23aacc253b064419
STadas <saltenis.tadas@gmail.com>
2022-04-10 17:56:02 +0300

    Remove ISavable and ILoadable

--- src/interfaces/ISavableLoadable.hpp
+++ src/interfaces/ISavableLoadable.hpp
@@ -1,15 +1,12 @@
 #pragma once
 
-#include "interfaces/ILoadable.hpp"
-#include "interfaces/ISavable.hpp"
-
-class ISavableLoadable : public ISavable, public ILoadable
+class ISavableLoadable
 {
 public:
     ISavableLoadable()
     {
     }
 
-    virtual void save() override = 0;
-    virtual void load() override = 0;
+    virtual void save() = 0;
+    virtual void load() = 0;
 };
